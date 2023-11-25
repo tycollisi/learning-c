75e8e6d9ff12e45280ef10fa710d3c81f09642af
Tyler Collisi <tcollisiedu@gmail.com>
2023-11-25 13:34:48 -0500

    adding dataTypes example

---
+++ misc-examples/src/dataTypesExample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int integerVar = 100;
+  float floatingVar = 331.79;
+  double doubleVar = 844e+11;
+  _Bool boolVar = 0;
+
+  printf("integerVar: %d\n floatingVar: %f\n doubleVar %lf\n boolVar: %d\n", integerVar, floatingVar, doubleVar, boolVar);
+
+  return 0;
+}
